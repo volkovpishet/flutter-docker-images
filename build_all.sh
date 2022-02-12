f0db7b1b735aa4d44e8013ec1a30425d1d357c25
Andrey Volkov <vlkv.andrey@gmail.com>
2022-02-12 19:54:37 +0400

    Disable caching

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,8 @@
-docker-compose --env-file .env -f base/docker-compose.yml build --progress plain
+docker-compose --env-file .env -f base/docker-compose.yml build --no-cache --progress plain
 docker-compose --env-file .env -f base/docker-compose.yml push
 
-docker-compose --env-file .env -f android/docker-compose.yml build --progress plain
+docker-compose --env-file .env -f android/docker-compose.yml build --no-cache --progress plain
 docker-compose --env-file .env -f android/docker-compose.yml push
 
-docker-compose --env-file .env -f linux/docker-compose.yml build --progress plain
+docker-compose --env-file .env -f linux/docker-compose.yml build --no-cache --progress plain
 docker-compose --env-file .env -f linux/docker-compose.yml push
